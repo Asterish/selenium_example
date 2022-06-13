ce572195e274e0bda3d225190086689230fbb3a2
Asterish <30481246+Asterish@users.noreply.github.com>
2022-06-13 01:02:00 -0400

    add basic nodejs example

--- nodejs/yahoo_node_example.js
+++ nodejs/yahoo_node_example.js
@@ -1,1 +1,19 @@
- 
+const {Builder, By, Key} = require('selenium-webdriver');
+
+(async function openFirefoxTest() {
+  try {
+    let driver = await new Builder().forBrowser('firefox').build();
+    await driver.get('https://www.yahoo.com');
+    await driver.findElement(By.name('p')).sendKeys('seleniumhq' + Key.RETURN);
+    await sleep(5000)
+    await driver.quit();
+  } catch (error) {
+    console.log(error)
+  }
+})();
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
